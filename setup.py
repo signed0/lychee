38d5e7675dd5dc742d5a130651540e1048fed0a8
Nathan Villaescusa <nathan.villaescusa@gmail.com>
2012-12-19 14:36:11 -0500

    Added code to read from the requirements file

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
+from os import path
 from setuptools import setup
 
+current_dir = path.dirname(path.abspath(__file__))
+requirements = open(path.join(current_dir, 'requirements.txt')).readlines()
+
 setup(name='lychee',
-	  version='1.0',
-	  description='A tool for analyzing HTTP requests',
-	  author='signed0',
-	  author_email='nathan@signedzero.com',
-	  packages=['lychee'],
-	  install_requires=['distribute', 'netifaces'],
-	  entry_points={
-		  'console_scripts': [
-			  'lychee = lychee.lychee:init_sudo'
-		  ]
-	  },
-	  zip_safe=False
-	  )
+      version='1.0',
+      description='A tool for analyzing HTTP requests',
+      author='signed0',
+      author_email='nathan@signedzero.com',
+      packages=['lychee'],
+      install_requires=requirements,
+      entry_points={
+          'console_scripts': [
+              'lychee = lychee.lychee:init_sudo'
+          ]
+      },
+      zip_safe=False
+      )
